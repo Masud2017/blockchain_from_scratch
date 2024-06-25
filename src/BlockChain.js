894abe3386f47d720a45aacb68a6a7a8eb155586
Masud karim <msmasud578@gmail.com>
2024-06-25 18:14:44 +0600

    updated some minor stuff

--- src/BlockChain.js
+++ src/BlockChain.js
@@ -1,22 +1,39 @@
-import {HashUtil} from "./HashUtil";
 
 class BlockChain {
 	construct (message) {
 		this.message = message;
-		this.extra = {}
+		this.title = "";
 		this.next = null;
 	}
 	
+	set setTitle (title) {
+		this.title = title;
+	}
 
-	addNew(message,...data) {
+	addNew(message,title) {
 		let newNode = new BlockChain(message);
-		if (data.length == > 0) {
-			// do the processing
-			data.forEach((el, index, arr) {
+		newNode.setTitle = title;
 
-			})
+		if (this.next == null) {
+			this.next = newNode;
 		} else {
-			// else do other processes
+			let temp = this;
+
+			while (temp.next != null) {
+				temp = temp.next;
+			}
+
+			temp.next = newNode;
 		}
+
 	}
 }
+
+
+let chain = new BlockChain("Hello world this is the message");
+chain.addNew("df","dafg");
+chain.setTitle = "The first genesis block"
+
+console.log("88888888888888888888888888888888888");
+console.log("inspecting the chain : ");
+console.log(chain);
