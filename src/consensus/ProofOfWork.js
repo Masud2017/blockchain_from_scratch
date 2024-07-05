589bc132b695096a69632508cd057b7fa007108e
Masud2017 <msmasud578@gmail.com>
2024-07-05 23:59:40 +0600

    added dependency for proof-of-work and code for proof of work

--- src/consensus/ProofOfWork.js
+++ src/consensus/ProofOfWork.js
@@ -1,23 +1,39 @@
-import {Consensus} from "./Consensus";
-
 /**
  * @author Md Masud karim
  * @copyright 2024
  * @license MIT
  * Implementation of Proof of work consensus algorithm used in Bitcoin
+ * Principle: A solution that is difficult to find but is easy to verify. -- Reference : https://www.geeksforgeeks.org/blockchain-proof-of-work-pow/
  */
 
-class ProofOfWork extends Consensus {
-    constructor(complexity = 10, nonce = 10) {
+
+import {createRequire} from "module";
+
+const require = createRequire(import.meta.url);
+const pow = require('proof-of-work');
+
+
+class ProofOfWork {
+    /**
+     * Make sure that the nonce prefix is unique to avoid attack like: reuse nonce attack
+     * @param {*} complexity 
+     * @param {*} prefix 
+     */
+    constructor(complexity = 10, prefix = "abcd") {
         this.complexity = complexity;
-        this.nonce = nonce;
+        this.prefix = Buffer.from(prefix, 'hex');
+        this.solver = new pow.Solver();
     }
 
     set setComplexity(complexity) {
         this.complexity = complexity;
     }
-    set setNone(nonce) {
-        this.nonce = nonce;
+    set setPrefix(prefix) {
+        this.prefix = prefix;
+    }
+
+    getNonce() {
+        return solver.solve(this.complexity, this.prefix);
     }
 }
 
